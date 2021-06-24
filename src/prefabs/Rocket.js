84d772387b1f89daa6a11573092f52662c748378
vderbier <victor.derbier@gmail.com>
2021-06-24 16:07:49 -0700

    movement and collision

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,5 +5,35 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         // add object to existing scene
         scene.add.existing(this);
+        this.isFiring = false;     // is the rocket firing
+        this.moveSpeed = 2;        // pixels per frame
+    }
+
+    update() {
+        // left/right movement
+        if (!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
+            }
+        }
+        // fire button
+        if (Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+        }
+        // if fired, move up
+        if (this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+        // reset on miss
+        if (this.y <= borderUISize * 3 + borderPadding) {
+            this.reset();
+        }
+    }
+
+    reset() {
+        this.isFiring = false;
+            this.y = game.config.height - borderUISize - borderPadding;
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -24,9 +24,49 @@ class Play extends Phaser.Scene {
 
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+        // add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
+        
+        // define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update() {
         this.starfield.tilePositionX -= 4;
+        this.p1Rocket.update();
+        this.ship01.update();
+        this.ship02.update();
+        this.ship03.update();
+
+        // check collisions
+        if (this.checkCollision(this.p1Rocket, this.ship03)) {
+            this.p1Rocket.reset();
+            this.ship03.reset();
+        }
+
+        if (this.checkCollision(this.p1Rocket, this.ship02)) {
+            this.p1Rocket.reset();   
+            this.ship02.reset();        
+        }
+
+        if (this.checkCollision(this.p1Rocket, this.ship01)) {
+            this.p1Rocket.reset(); 
+            this.ship01.reset();
+        }
+    }
+
+    checkCollision(rocket, ship) {
+        if (rocket.x < ship.x + ship.width && 
+            rocket.x + rocket.width > ship.x + ship.width &&
+            rocket.y < ship.y + ship.height &&
+            rocket.y + rocket.height > ship.y) {
+                return true;
+        }
+        return false;
     }
 }
