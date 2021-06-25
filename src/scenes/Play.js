2780f5a31e365997a509d8bb93468bd6fe3c6c63
vderbier <victor.derbier@gmail.com>
2021-06-24 18:38:41 -0700

    Game is finished

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -67,10 +67,10 @@ class Play extends Phaser.Scene {
 
         // 60 seconds play clock
         scoreConfig.fixedWidth = 0; 
-        this.clock = this.time.delayedCall(60000, () => {
-            this.add.text(game.config.width/2, game.config.height/2, "GAME OVER", scoreConfig).setOrigin(0, 0);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
-            this.gameOver = true;  // To know when to stop the game.
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
+            this.add.text(game.config.width/2, game.config.height/2, "GAME OVER", scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or ← for Menu', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
         }, null, this);
     }
 
@@ -78,6 +78,10 @@ class Play extends Phaser.Scene {
         // check key input for restart
         if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
             this.scene.restart();
+        }
+
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start("menuScene");
         }
 
         this.starfield.tilePositionX -= 4;
